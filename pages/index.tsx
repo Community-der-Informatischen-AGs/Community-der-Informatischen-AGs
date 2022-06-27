3f877b0ce856774a4198984e479590cede85a88a
Rui Zhang <totallyturing@gmail.com>
2022-06-27 11:39:43 +0200

    ready for testing deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,45 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/home/home.module.css'
+import globalStyles from "../styles/globals.module.css";
 
 import { Contentful } from '../lib/contentful/api'
+
+import TypeAnimation from "react-type-animation";
 
 // ! https://www.npmjs.com/package/@contentful/rich-text-react-renderer for rendering rich text
 
 const Home: NextPage = (props) => {
 
   return <>
-    <h1>Jugend Entwickelt Informatik</h1>
-  </>
+    <Head>
+      <title>Jugend Entwickelt Informatik</title>
+    </Head>
+    <main>
+      <section className={styles.headingSection}>
+        <h1 className={globalStyles.heading + " " + styles.heading}>
+          <span className={styles.seeThrough}>
+            Die
+          </span>
+          <span>
+            Jugend
+          </span>
+          <span>
+            Entwickelt
+          </span>
+          <span>
+            Informatik
+            <span className={styles.seeThrough}>
+             -
+            </span>
+          </span>
+          <span className={styles.seeThrough}>
+            Projekte
+          </span>
+        </h1>
+      </section>
+    </main>
+  </>;
 }
 
 export default Home
