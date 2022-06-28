f394a3275fd6f78f5497796e168db47e0a5f63d8
Rui Zhang <totallyturing@gmail.com>
2022-06-28 14:40:37 +0200

    meta tags added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,14 @@ export default function Document() {
     <Html lang="de">
       <Head>
         <link rel="shortcut icon" href="/favicon.ico"></link>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Die Webseite des Jugend-Entwickelt-Software-Projekts"
+        />
       </Head>
       <body>
         <Main />
