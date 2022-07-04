60f80bbed5fb3640333acc12ea13ec478bd5be1e
Rui Zhang <rui.zhang@avenga.com>
2022-07-04 22:22:40 +0200

    minor errorfix in document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />    
-        <meta http-equiv="X-UA-Compatible" content="ie=edge"></meta>
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge"></meta>
         <meta name="theme-color" content="#000000" />
         <meta
           name="description"
