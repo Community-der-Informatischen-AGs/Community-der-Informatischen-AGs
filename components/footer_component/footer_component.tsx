3cc6caf7379ec09f9a5dfddeaf13a30ad91aae45
Rui Zhang <rui.zhang@avenga.com>
2022-07-15 13:28:03 +0200

    footer social media logos added

--- components/footer_component/footer_component.tsx
+++ components/footer_component/footer_component.tsx
@@ -1,9 +1,29 @@
+import Link from "next/link"
+import {
+  Envelope,
+  GithubLogo,
+  InstagramLogo,
+  YoutubeLogo,
+} from "phosphor-react"
 import styles from "./footer_component.module.scss"
 
 export const Footer = () => {
   return (
     <footer>
-      <h6>2022 © Jugend-Entwickel-Digital </h6>
+      <section className={styles.socialMedia}>
+        {/* bunch of icons here */}
+        <InstagramLogo size={32} weight="thin" />
+        <YoutubeLogo size={32} weight="thin" />
+        <Envelope size={32} weight="thin" />
+        <GithubLogo size={32} weight="thin" />
+      </section>
+      <section className={styles.links}>
+        <Link href={}>Impressum</Link>
+        <Link href={}>Annette-Entwickelt-Digital</Link>
+        <Link href={}>Schloß-Entwickelt-Digital</Link>
+        <Link href={}>Kobi-Entwickelt-Digital</Link>
+      </section>
+      <h6>2022 © Jugend-Entwickelt-Digital</h6>
     </footer>
   )
 }
