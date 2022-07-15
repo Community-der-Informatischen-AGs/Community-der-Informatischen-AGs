d35b732c424a9a7de06b4391518858e5cbcbf352
Rui Zhang <rui.zhang@avenga.com>
2022-07-15 13:24:20 +0200

    simple html structure implemented

--- components/footer_component/footer_component.tsx
+++ components/footer_component/footer_component.tsx
@@ -1,3 +1,9 @@
 import styles from "./footer_component.module.scss"
 
-export const Footer = () => {}
+export const Footer = () => {
+  return (
+    <footer>
+      <h6>2022 © Jugend-Entwickel-Digital </h6>
+    </footer>
+  )
+}
