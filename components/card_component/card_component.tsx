e0e2fbdf8b77a351454edfd57333f16baa50024d
Rui Zhang <totallyturing@gmail.com>
2022-09-19 20:38:08 +0200

    exchanged img with next image in carousel component

--- components/card_component/card_component.tsx
+++ components/card_component/card_component.tsx
@@ -1,21 +1,21 @@
 import { useRouter } from "next/router"
 import {
-  HasOptionalImage,
   HasOptionalStyleSheet,
-  SCSSStyleSheet,
+  ImageData,
 } from "../../lib/utils/types"
 import styles from "./card_component.module.scss"
+import globalStyles from "./../../styles/globals.module.scss"
 
 import cn from "classnames"
 import { processOptStyleSheet } from "../../lib/utils/functions"
+import Image from "next/image"
 
 // TODO: use Next Images
 interface CardComponentProps extends HasOptionalStyleSheet {
-  imageSource: string
-  imageAlt: string
+  image: ImageData
   title: string
   text: string
-  link: string
+  link?: string
   className?: string
 }
 
@@ -32,10 +32,24 @@ export const CardComponent = (p: CardComponentProps) => {
         stylesheet.cardComponent
       )}
       onClick={() => {
-        router.push(p.link)
+        if (p.link != undefined) {
+          router.push(p.link)
+        }
       }}
     >
-      <img src={p.imageSource} alt={p.imageAlt} />
+      <section
+        className={cn(
+          styles.imageSection,
+          stylesheet.imageSection
+        )}
+      >
+        <Image
+          src={p.image.url}
+          alt={p.image.title}
+          height={p.image.height}
+          width={p.image.width}
+        />
+      </section>
       <section
         className={cn(
           styles.textSection,
